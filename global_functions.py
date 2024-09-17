4d900961c74a543faa14f74cf36fe09a50f178f0
Yousaf Hashmi <112760805+yousafhashmi98@users.noreply.github.com>
2024-09-17 15:00:27 +0500

    modified 12

--- global_functions.py
+++ global_functions.py
@@ -3,17 +3,26 @@ import os
 import numpy as np
 
 
+# def convert_numpy_to_python(results):
+#     def convert_item(item):
+#         if isinstance(item, np.ndarray):
+#             return item.tolist()
+#         elif isinstance(item, (np.generic, np.integer, np.floating)):
+#             return item.item()  
+#         elif isinstance(item, list):
+#             return [convert_item(sub_item) for sub_item in item]
+#         else:
+#             return item
+#     return [convert_item(result) for result in results]
+
 def convert_numpy_to_python(results):
-    def convert_item(item):
-        if isinstance(item, np.ndarray):
-            return item.tolist()
-        elif isinstance(item, (np.generic, np.integer, np.floating)):
-            return item.item()  
-        elif isinstance(item, list):
-            return [convert_item(sub_item) for sub_item in item]
-        else:
-            return item
-    return [convert_item(result) for result in results]
+    converted_data = []
+    for item in results:
+        coordinates, text, score = item
+        coordinates = [[int(x), int(y)] for [x, y] in coordinates]
+        score = float(score)
+        converted_data.append((coordinates, text, score))
+    return converted_data
 
 
 def write_file(file:UploadFile):
